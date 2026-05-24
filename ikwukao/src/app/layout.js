66bfc0ea576ba1054fabfa2f6d9667c47c998034
Ikwuka Okoye <ikwukao@proton.me>
2026-05-24 01:10:33 +0100

    Improve metadata

--- ikwukao/src/app/layout.js
+++ ikwukao/src/app/layout.js
@@ -12,12 +12,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Ikwukao.dev — Backend Engineer & AI Builder",
-  description: `Ikwukao.dev is the personal website of Ikwuka Okoye, 
-                a passionate backend engineer and AI builder. 
-                Explore my projects, experience, and get in 
-                touch to collaborate on innovative tech solutions.
-              `,
+  title: "Ikwukao.dev | Backend Engineer",
+  description: "Backend Engineer specializing in Python, Go, APIs, Linux, and AI systems.",
 };
 
 export default function RootLayout({ children }) {
